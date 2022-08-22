960cec90e905815635d1436bebab1091b21c143c
gutaam.gt <gutaam.gt@gmail.com>
2022-08-22 07:01:01 +0700

    adjust

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ $config['modules_locations'] = array(
 // @$config['base_url']  = ((isset($_SERVER['HTTPS'])&& $_SERVER['HTTPS'] =="on") ? "https" : "http");
 // @$config['base_url'] .= "://" .$_SERVER['HTTP_HOST'];
 // @$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-$config['base_url'] = "http://k3ig20korlantas.id:3001/";
+$config['base_url'] = "http://k3ig20korlantas.id/korlantas-web/";
 
 /*
 |--------------------------------------------------------------------------
