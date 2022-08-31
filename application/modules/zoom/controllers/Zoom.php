66c2371b3d5fa2986bde58ca98edee248f3e22ce
Muhamad Muslih Himawan <muslih259@gmail.com>
2022-08-31 22:02:39 +0700

    zoom

--- application/modules/zoom/controllers/Zoom.php
+++ application/modules/zoom/controllers/Zoom.php
@@ -1,37 +1,38 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Zoom extends MY_Controller {
+class Zoom extends MY_Controller
+{
 
-    public function __construct(){
-        parent::__construct(); 
+    public function __construct()
+    {
+        parent::__construct();
         $this->load->helper("logged_helper");
-    } 
-	
-	public function index()
-	{
-		
-		// $headers = [
+    }
+
+    public function index()
+    {
+
+        // $headers = [
         //     'Token' => $this->session->userdata['token'],    
         // ];
 
         $page_content["css"] = '';
         $page_content["js"] = '';
-        $page_content["title"] = "zoom";  
-		
-        if($this->session->userdata['role'] == 'G20'){
+        $page_content["title"] = "zoom";
+
+        if ($this->session->userdata['role'] == 'G20') {
             $page_content["page"] = "zoom/list_g20";
-        }else if($this->session->userdata['role'] == 'Korlantas'){
-            $page_content["page"] = "zoom/list_view";
-        }else if($this->session->userdata['role'] == 'Kapolda'){
-            $page_content["page"] = "zoom/list_view";
-        }else if($this->session->userdata['role'] == 'Polres'){
-            $page_content["page"] = "zoom/list_view";
-        } 
- 
-		
+        } else if ($this->session->userdata['role'] == 'Korlantas') {
+            $page_content["page"] = "zoom/list_korlantas";
+        } else if ($this->session->userdata['role'] == 'Kapolda') {
+            $page_content["page"] = "zoom/list_kapolda";
+        } else if ($this->session->userdata['role'] == 'Polres') {
+            $page_content["page"] = "zoom/list_polres";
+        }
+
+
         $page_content["data"] = '';
         $this->templates->loadTemplate($page_content);
-    } 
-  
+    }
 }
