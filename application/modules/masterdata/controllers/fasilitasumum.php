0e30112fedbebedc7adf460260a20b0f8c2d439a
aliwafa16 <aliwafamuhamad16@gmail.com>
2022-08-31 16:58:02 +0700

    master data

---
+++ application/modules/masterdata/controllers/fasilitasumum.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class fasilitasumum extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("logged_helper");
+    }
+
+    public function index()
+    {
+
+        // $headers = [
+        //     'Token' => $this->session->userdata['token'],    
+        // ];
+
+        $page_content["css"] = '';
+        $page_content["js"] = '';
+        $page_content["title"] = "Fasilitas Umum";
+
+        if ($this->session->userdata['role'] == 'G20') {
+            $page_content["page"] = "dashboard/dashboard_g20";
+        } else if ($this->session->userdata['role'] == 'Korlantas') {
+            $page_content["page"] = "masterdata/Korlantas/fasum_view";
+        } else if ($this->session->userdata['role'] == 'Kapolda') {
+            $page_content["page"] = "masterdata/Kapolda/fasum_view";
+        } else if ($this->session->userdata['role'] == 'Polres') {
+            $page_content["page"] = "masterdata/Polres/fasum_view";
+        }
+
+
+
+
+        $page_content["data"] = '';
+        $this->templates->loadTemplate($page_content);
+    }
+}
---
+++ application/modules/masterdata/controllers/simkeliling.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class simkeliling extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("logged_helper");
+    }
+
+    public function index()
+    {
+
+        // $headers = [
+        //     'Token' => $this->session->userdata['token'],    
+        // ];
+
+        $page_content["css"] = '';
+        $page_content["js"] = '';
+        $page_content["title"] = "Sim Keliling";
+
+        if ($this->session->userdata['role'] == 'G20') {
+            $page_content["page"] = "dashboard/dashboard_g20";
+        } else if ($this->session->userdata['role'] == 'Korlantas') {
+            $page_content["page"] = "masterdata/Korlantas/simkel_view";
+        } else if ($this->session->userdata['role'] == 'Kapolda') {
+            $page_content["page"] = "masterdata/Kapolda/simkel_view";
+        } else if ($this->session->userdata['role'] == 'Polres') {
+            $page_content["page"] = "masterdata/Polres/simkel_view";
+        }
+
+
+
+
+        $page_content["data"] = '';
+        $this->templates->loadTemplate($page_content);
+    }
+}
