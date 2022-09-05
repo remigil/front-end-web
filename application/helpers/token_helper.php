f5782a9653f3cbdc6e38d4ff38650190a7bf462f
gutaam.gt <gutaam.gt@gmail.com>
2022-09-05 18:52:40 +0700

    ser

--- application/helpers/token_helper.php
+++ application/helpers/token_helper.php
@@ -6,7 +6,7 @@ use GuzzleHttp\Client;
 if (!function_exists('token')) {  
     function token(){
         $client = new Client();
-        $request = $client->request('POST', 'http://localhost:3001/v1/', [
+        $request = $client->request('POST', 'http://k3ig20korlantas.id:3001/v1/', [
             'form_params' => [
                 'username' => 'admin',
                 'password' => '1legalitas!!'
