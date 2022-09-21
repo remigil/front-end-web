7a4f233c542ba66e86bef21c0efac79366cba542
gutaam.gt <gutaam.gt@gmail.com>
2022-09-21 21:02:35 +0700

    ido

---
+++ application/modules/masterdata/controllers/Bahan_bakar.php
@@ -0,0 +1,230 @@
+
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Bahan_bakar extends MY_Controller
+{
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper("logged_helper"); 
+        $this->load->model('operasi/m_bahan_bakar'); 
+    }
+
+    public function index()
+    {
+
+        $headers = [
+            'Authorization' => $this->session->userdata['token'],    
+        ];
+
+        $page_content["css"] = '';
+        $page_content["js"] = '';
+        $page_content["title"] = "Operasi";
+
+        if ($this->session->userdata['role'] == 'G20') {
+            $page_content["page"] = "operasi/G20/bahan_bakar";
+        } else if ($this->session->userdata['role'] == 'Korlantas') {
+            $page_content["page"] = "operasi/Korlantas/bahan_bakar_korlantas";
+        } else if ($this->session->userdata['role'] == 'Kapolda') {
+            $page_content["page"] = "operasi/Kapolda/bahan_bakar_kapolda";
+        } else if ($this->session->userdata['role'] == 'Polres') {
+            $page_content["page"] = "operasi/Polres/bahan_bakar_polres";
+        } 
+
+
+        $page_content["data"] = '';
+        $this->templates->loadTemplate($page_content);
+    }
+
+    public function serverSideTable() 
+    {  
+        $postData = $this->input->post();   
+        $data = $this->m_bahan_bakar->get_datatables($postData);  
+		echo json_encode($data); 
+    }
+
+    public function store() 
+    {  
+        $headers = [ 
+            'Authorization' => $this->session->userdata['token'],  
+        ]; 
+        $input      = $this->input->post(); 
+        $dummy = [
+            [
+                'name' => 'name_fuelVehicle',
+                'contents' => $input['nobahan_bakar'],
+            ],
+            [
+                'name' => 'status_fuelVehicle',
+                'contents' => $input['jenisbahan_bakar'],
+            ] 
+        ];
+
+        $data = guzzle_request('POST', 'fuel_vehicle/add', [ 
+            'multipart' => $dummy, 
+            'headers' => $headers 
+        ]);
+
+        if($data['isSuccess'] == true){  
+            $res = array(
+                'status' => true,
+                'message' => 'Berhasil tambah data.',
+                'data' => $data
+            );
+        }else{
+            $res = array(
+                'status' => false,
+                'message' => 'Gagal tambah data.',
+                'data' => $data
+            );
+        }
+        
+        echo json_encode($res);
+
+    }
+
+    public function Detail($id)
+    {
+
+        $headers = [
+            'Authorization' => $this->session->userdata['token'],    
+        ];
+
+        $page_content["css"] = '';
+        $page_content["js"] = '';
+        $page_content["title"] = "Operasi";
+
+        if ($this->session->userdata['role'] == 'G20') {
+            $page_content["page"] = "operasi/G20/detail_bahan_bakar";
+        } else if ($this->session->userdata['role'] == 'Korlantas') {
+            $page_content["page"] = "operasi/Korlantas/detail_bahan_bakar_korlantas";
+        } else if ($this->session->userdata['role'] == 'Kapolda') {
+            $page_content["page"] = "operasi/Kapolda/detail_bahan_bakar_kapolda";
+        } else if ($this->session->userdata['role'] == 'Polres') {
+            $page_content["page"] = "operasi/Polres/detail_bahan_bakar_polres";
+        }
+
+
+        $getDetail = guzzle_request('GET', 'fuel_vehicle/getId/'.$id.'', [  
+            'headers' => $headers 
+        ]);
+        $data['getDetail'] = $getDetail['data'];
+        // echo json_encode($data['getDetail']['data']['no_vehicle']);
+        // die;
+
+        $page_content["data"] = $data;
+        $this->templates->loadTemplate($page_content);
+    }
+    public function Edit($id)
+    {
+
+        $headers = [
+            'Authorization' => $this->session->userdata['token'],    
+        ];
+
+        $page_content["css"] = '';
+        $page_content["js"] = '';
+        $page_content["title"] = "Operasi";
+
+        if ($this->session->userdata['role'] == 'G20') {
+            $page_content["page"] = "operasi/G20/edit_bahan_bakar";
+        } else if ($this->session->userdata['role'] == 'Korlantas') {
+            $page_content["page"] = "operasi/Korlantas/edit_bahan_bakar_korlantas";
+        } else if ($this->session->userdata['role'] == 'Kapolda') {
+            $page_content["page"] = "operasi/Kapolda/edit_bahan_bakar_kapolda";
+        } else if ($this->session->userdata['role'] == 'Polres') {
+            $page_content["page"] = "operasi/Polres/edit_bahan_bakar_polres";
+        }
+
+
+        $getDetail = guzzle_request('GET', 'fuel_vehicle/getId/'.$id.'', [  
+            'headers' => $headers 
+        ]);
+        $data['getDetail'] = $getDetail['data'];
+
+        $page_content["data"] = $data;
+        $this->templates->loadTemplate($page_content);
+    }
+
+
+    public function storeEdit() 
+    {  
+        $headers = [ 
+            'Authorization' => $this->session->userdata['token'],  
+        ]; 
+        $input      = $this->input->post(); 
+        $dummy = [
+            [
+                'name' => 'name_fuelVehicle',
+                'contents' => $input['nobahan_bakar'],
+            ],
+            [
+                'name' => 'status_fuelVehicle',
+                'contents' => $input['jenisbahan_bakar'],
+            ] 
+        ];
+
+        $data = guzzle_request('PUT', 'fuel_vehicle/edit/'.$input['id'].'', [ 
+            'multipart' => $dummy, 
+            'headers' => $headers 
+        ]);
+
+        if($data['isSuccess'] == true){  
+            $res = array(
+                'status' => true,
+                'message' => 'Berhasil edit data.',
+                'data' => $data
+            );
+        }else{
+            $res = array(
+                'status' => false,
+                'message' => 'Gagal edit data.',
+                'data' => $data
+            );
+        }
+        
+        echo json_encode($res);
+
+    }
+
+
+    public function delete() 
+    {  
+        $headers = [ 
+            'Authorization' => $this->session->userdata['token'],  
+        ];  
+
+        $input      = $this->input->post(); 
+        $dummy = [
+            [
+                'name' => 'id',
+                'contents' => $input['id'],
+            ] 
+        ];
+
+        $data = guzzle_request('DELETE', 'fuel_vehicle/delete', [ 
+            'multipart' => $dummy, 
+            'headers' => $headers 
+        ]);
+
+        if($data['isSuccess'] == true){  
+            $res = array(
+                'status' => true,
+                'message' => 'Berhasil hapus data.',
+                'data' => $data
+            );
+        }else{
+            $res = array(
+                'status' => false,
+                'message' => 'Gagal hapus data.',
+                'data' => $data
+            );
+        }
+        
+        echo json_encode($res);
+
+    }
+
+}
