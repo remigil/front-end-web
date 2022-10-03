d1f6f0775b1e7df5188ad194fb9f0604f5f85ef4
gutaam.gt <gutaam.gt@gmail.com>
2022-10-03 21:05:40 +0700

    ido

--- application/helpers/guzzle_request_helper.php
+++ application/helpers/guzzle_request_helper.php
@@ -26,8 +26,8 @@ if (!function_exists('guzzle_request')) {
 
     function urlApi()
     {
-        return 'http://localhost:3001/v1/';
-        // return 'http://k3ig20korlantas.id:3001/v1/'; 
+        return 'http://localhost:3001/uploads/';
+        // return 'http://k3ig20korlantas.id:3001/uploads/'; 
     }
 }
 
@@ -43,8 +43,8 @@ if (!function_exists('guzzle_requestTracking')) {
     function guzzle_requestTracking($method, $uri, $body)
     {
         $client = new Client();
-        // $request = $client->request($method, 'http://localhost:3001/track-location/' . $uri, $body);
-        $request = $client->request($method, 'http://k3ig20korlantas.id:3001/track-location/' . $uri, $body);
+        $request = $client->request($method, 'http://localhost:3001/track-location/' . $uri, $body);
+        // $request = $client->request($method, 'http://k3ig20korlantas.id:3001/track-location/' . $uri, $body);
         $response = $request->getBody();
         $data = json_decode($response, true);
 
