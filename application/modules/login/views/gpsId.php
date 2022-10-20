70210d6215b05742f71a1fb405559277d96964cb
gutaam.gt <gutaam.gt@gmail.com>
2022-10-20 19:33:01 +0700

    ada

--- application/modules/login/views/gpsId.php
+++ application/modules/login/views/gpsId.php
@@ -7,12 +7,31 @@
 </head>
 <div id="player"></div>
 <script>
-function file_get_contents(filename) {
-    fetch(filename).then((resp) => resp.text()).then(function(data) {
-        return data;
-    });
-}
-file_get_contents('php://input');
+  fetch('<?php echo base_url()?>login/gpsid', {
+      method: 'post',
+      mode: "same-origin",
+      headers: {
+          "X-Requested-With": "XMLHttpRequest", 
+          "Content-type": "application/x-www-form-urlencoded"
+      },
+      credentials: "same-origin",
+      body: {
+        "VehicleId":"862476051307543",
+        "VehicleNumber":"b 1925 WKX",
+        "DatetimeUTC":"2022-01-13 08:05:51",
+        "GpsLocation":"Jalan Petojo Sabangan VI, RW 04, Petojo Selatan, Gambir, Jakarta Pusat, Daerah Khusus Ibukota Jakarta, 10160, Indonesia",
+        "Lon":"106.8130966",
+        "Lat":"-6.1777583",
+        "Speed":"0",
+        "Direction":"0",
+        "Engine":"OFF",
+        "Odometer":146247,
+        "Car_status" : "START"
+      }
+  }).then(response => response.text())
+  .then(output => {
+    // do stuff
+  });
 </script>
 </body>
 </html>
