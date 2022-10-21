b4d5885ee888e3606787ccdefb15b1e4b823ae29
gutaam.gt <gutaam.gt@gmail.com>
2022-10-21 19:47:12 +0700

    da

--- application/modules/gmaps/views/list_g20.php
+++ application/modules/gmaps/views/list_g20.php
@@ -1,6 +1,6 @@
 <div class="mapouter">
   <div class="gmap_canvas">
-    <iframe style="width: 100%;" height="618" id="gmap_canvas" src="https://maps.google.com/maps?q=bali&t=k&z=9&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+    <iframe style="width: 100%;" height="618" id="gmap_canvas" src="https://maps.google.com/maps?q=bali&t=k&z=11&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
     <a href="https://123movies-to.org"></a>
     <br>
     <style>.mapouter{position:relative;text-align:right;height:618px;width:100%;}</style>
