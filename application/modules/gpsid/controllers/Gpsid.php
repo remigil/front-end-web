5e01ed55df92e09bc1596d2a299b9edfe3c55ad3
gutaam.gt <gutaam.gt@gmail.com>
2022-10-22 16:58:14 +0700

    d

--- application/modules/gpsid/controllers/Gpsid.php
+++ application/modules/gpsid/controllers/Gpsid.php
@@ -20,20 +20,26 @@ class Gpsid extends MX_Controller
         $data = file_get_contents('php://input');
         $param = json_decode($data, true);
 
-        $VehicleId = $param->VehicleId; //862476051307543
-        $VehicleNumber = $param->VehicleNumber; //b 1925 WKX
-        $DatetimeUTC = $param->DatetimeUTC; 
-        $GpsLocation = $param->GpsLocation;
-        $Lon = $param->Lon;
-        $Lat = $param->Lat;
-        $Speed = $param->Speed;
-        $Direction = $param->Direction;
-        $Engine = $param->Engine;
-        $Odometer = $param->Odometer;
-        $Car_status= $param->Car_status;
-  
-        // $dummy['VehicleId'] = $param->VehicleId;
-        file_put_contents('./dataVendor/gpsId/gpsId_'.date('Y-m-d').'.json', $param, FILE_APPEND);
+        // $VehicleId = $param->VehicleId; //862476051307543
+        // $VehicleNumber = $param->VehicleNumber; //b 1925 WKX
+        // $DatetimeUTC = $param->DatetimeUTC; 
+        // $GpsLocation = $param->GpsLocation;
+        // $Lon = $param->Lon;
+        // $Lat = $param->Lat;
+        // $Speed = $param->Speed;
+        // $Direction = $param->Direction;
+        // $Engine = $param->Engine;
+        // $Odometer = $param->Odometer;
+        // $Car_status= $param->Car_status; 
+
+        $res = array(
+            'VehicleId' => $param->VehicleId, 
+            'VehicleNumber' => $param->VehicleNumber, 
+            'Lon' => $param->Lon, 
+            'Lat' => $param->Lat, 
+            'Speed' => $param->Speed, 
+        );
+        file_put_contents('./dataVendor/gpsId/gpsId_'.date('Y-m-d').'.json', $res, FILE_APPEND);
         $ini1 = file_get_contents('./dataVendor/gpsId/gpsId_'.date('Y-m-d').'.json');
         echo $ini1;
         die;
