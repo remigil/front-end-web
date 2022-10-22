19e0a639c7b2b023cc142c62567630a0590cf8e1
gutaam.gt <gutaam.gt@gmail.com>
2022-10-22 17:34:38 +0700

    da

--- application/modules/gpsid/controllers/Gpsid.php
+++ application/modules/gpsid/controllers/Gpsid.php
@@ -37,8 +37,8 @@ class Gpsid extends MX_Controller
 
         
         // file_put_contents("./dataVendor/gpsId.json", "");
-        file_put_contents('./dataVendor/gpsId.json', [str_replace('}','},',$data)], FILE_APPEND);
-        $ini1 = file_get_contents('./dataVendor/gpsId.json');
+        file_put_contents('./dataVendor/gpsId.json', str_replace('}','},',$data), FILE_APPEND);
+        $ini1 = json_decode(file_get_contents('./dataVendor/gpsId.json'));
         echo json_encode($ini1);
         die;
 
