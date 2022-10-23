633e416c93c7dc9278a319fece92939d54f83dc5
FADHLUN <zafadh860@gmail.com>
2022-10-23 17:11:55 +0700

    Kakor detail, anev

--- application/modules/data_harian_opsus/views/Kakor/data_harian_opsus_view.php
+++ application/modules/data_harian_opsus/views/Kakor/data_harian_opsus_view.php
@@ -8,47 +8,96 @@
 </nav>
 <!-- </div> -->
 
-	<div class="card-header card-headline">
-		<div class="row align-items-center">
-			<div class="col-md-6">
-				<!-- <h2 class="text-white"><?= strtoupper($data['headline'])  ?></h2> -->
-				<h2>headline</h2>
-			</div>
-			<div class="col-md-6">
-				<!-- <p class="text-end mx-4 text-dark fs-6 fw-light"><?= ucwords('last update ' . date('j F, Y'))  ?></p> -->
-				<p>last update</p>
-			</div>
-		</div>
-	</div>
+<div class="card-header card-headline">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <!-- <h2 class="text-white"><?= strtoupper($data['headline'])  ?></h2> -->
+            <h2 style="color: white; text-align:center;">DAFTAR DATA HARIAN OPERASI KHUSUS</h2>
+        </div>
+    </div>
+</div>
 
-	<div class="page mt-5">
-    
-    <div class="card">
+<div class="page mt-5">
+    <div class="page mt-5">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <!-- <div style="width: 25%; float:right; border: 1px; margin-bottom: 10px">
+                <input type="search" class="form-control light-table-filter" data-table="table-hower" placeholder="Search..." />
+            </div> -->
+                <table id="datatable" class="table dt-responsive w-100" style="width: 800px;
+        margin: 0 auto;">
+                    <thead>
+                        <tr class="table-info">
+                            <th>No</th>
+                            <th>Nama Operasi</th>
+                            <th>Tanggal Mulai</th>
+                            <th>Tanggal Berakhir</th>
+                            <th>Laporan Harian</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <th>First</th>
+                            <th>Last</th>
+                            <th>Tanggal</th>
+                            <th>
+                                <a href="<?php echo base_url(); ?>data_harian_opsus/Data_harian_opsus/detaildata/"><button class="btn btn-sm btn-primary">Detail</button></a>
+                            </th>
+                        </tr>
+                        <tr class="table-light">
+                            <th>2</th>
+                            <th>First</th>
+                            <th>Last</th>
+                            <th>Tanggal</th>
+                            <th>
+                                <a href="<?php echo base_url(); ?>data_harian_opsus/Data_harian_opsus/detaildata/"><button class="btn btn-sm btn-primary">Detail</button></a>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>3</th>
+                            <th>First</th>
+                            <th>Last</th>
+                            <th>Tanggal</th>
+                            <th>
+                                <a href="<?php echo base_url(); ?>data_harian_opsus/Data_harian_opsus/detaildata/"><button class="btn btn-sm btn-primary">Detail</button></a>
+                            </th>
+                        </tr>
+                        <tr class="table-light">
+                            <th>4</th>
+                            <th>First</th>
+                            <th>Last</th>
+                            <th>Tanggal</th>
+                            <th>
+                                <a href="<?php echo base_url(); ?>data_harian_opsus/Data_harian_opsus/detaildata/"><button class="btn btn-sm btn-primary">Detail</button></a>
+                            </th>
+                        </tr>
+                        <tr>
+                            <th>5</th>
+                            <th>First</th>
+                            <th>Last</th>
+                            <th>Tanggal</th>
+                            <th>
+                                <a href="<?php echo base_url(); ?>data_harian_opsus/Data_harian_opsus/detaildata/"><button class="btn btn-sm btn-primary">Detail</button></a>
+                            </th>
+                        </tr>
+                        </thead>
+                </table>
 
-        <div class="card-body">
-
-            <table id="datatable" class="table dt-responsive w-100">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Nama Operasi</th>
-                        <th>Tanggal Mulai</th>
-                        <th>Tanggal Berakhir</th>
-                        <th>Laporan Harian</th>
-                    </tr>
-                </thead>
-                
-            </table>
-
+            </div>
         </div>
     </div>
 </div>
 
 <script>
-	$(document).ready(function() {
+    $(document).ready(function() {
+        $('#datatable').DataTable({
+            scrollX: true,
+        });
+
         $('.dropify').dropify();
 
-		$('[name=cordinate]').val('-1.5707209, 115.4875168');
+        $('[name=cordinate]').val('-1.5707209, 115.4875168');
         var initialCenter = [-1.5707209, 115.4875168];
         var initialZoom = 5;
         var googleStreet = L.tileLayer('https://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}', {
@@ -80,7 +129,7 @@
             zoomControl: false,
             layers: [googleStreet]
         }).setView(initialCenter, initialZoom);
-  
+
         var baseMaps = {
             "Google Map Street": googleStreet,
             "Google Map Satelite": googleSatelite,
@@ -94,8 +143,8 @@
         L.control.zoom({
             position: 'bottomleft'
         }).addTo(mapContainer);
-		
-	$('#myModal').on('shown.bs.modal', function() {
+
+        $('#myModal').on('shown.bs.modal', function() {
             mapContainer.invalidateSize();
 
             $('.dropify').dropify();
@@ -103,11 +152,11 @@
 
 
             let countlist = 0;
-            let list = ""; 
-            $('[name=address]').on("change", function (e) {
+            let list = "";
+            $('[name=address]').on("change", function(e) {
                 // console.log(this.value);
-                $.get(`https://nominatim.openstreetmap.org/search?format=json&q=${this.value}`, function(ress){
-                    console.log(ress);  
+                $.get(`https://nominatim.openstreetmap.org/search?format=json&q=${this.value}`, function(ress) {
+                    console.log(ress);
                     countlist = 0;
                     list = "";
                     ress.forEach(el => {
@@ -116,29 +165,29 @@
                         id="list${countlist}"   
                         data-alamat="${el.display_name}"
                         data-cords="${el.lat},${el.lon}" href="javascript:void(0)">${el.display_name}</a>`;
-                        $('#listAddress').html(list); 
-                    });  
+                        $('#listAddress').html(list);
+                    });
 
-                    if(list == ""){
+                    if (list == "") {
                         countlist = 0;
                         list = "";
-                        $('#listAddress').html(list); 
+                        $('#listAddress').html(list);
                     }
 
-                    
-                    for (let i = 0; i < ress.length; i++){ 
-                        $(`#list${i+1}`).click(function(){  
-                            var latlong =  $(this).data('cords').split(',');
-                            var latitude = parseFloat(latlong[0]);
-                            var longitude = parseFloat(latlong[1]); 
 
-							$("[name=latitude]").val(latitude);
-							$("[name=longitude]").val(longitude);
+                    for (let i = 0; i < ress.length; i++) {
+                        $(`#list${i+1}`).click(function() {
+                            var latlong = $(this).data('cords').split(',');
+                            var latitude = parseFloat(latlong[0]);
+                            var longitude = parseFloat(latlong[1]);
+
+                            $("[name=latitude]").val(latitude);
+                            $("[name=longitude]").val(longitude);
 
                             // console.log({a:latitude, b:longitude});
                             $('[name=address]').val($(this).data('alamat'));
                             $('[name=cordinate]').val($(this).data('cords'));
-                            mapContainer.flyTo([latitude, longitude], 17);    
+                            mapContainer.flyTo([latitude, longitude], 17);
                         });
                     }
                 });
@@ -146,39 +195,39 @@
             });
 
 
-            $('[name=cordinate]').on("change", function (e) {
+            $('[name=cordinate]').on("change", function(e) {
 
-                var cordLatLong =  this.value.split(','); 
-                var cordLat = parseFloat(cordLatLong[0]); 
-                var corLong = parseFloat(cordLatLong[1]); 
+                var cordLatLong = this.value.split(',');
+                var cordLat = parseFloat(cordLatLong[0]);
+                var corLong = parseFloat(cordLatLong[1]);
 
                 // console.log({a:cordLat, b:corLong});
 
-                $.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${cordLat}&lon=${corLong}`, function(data){
-                    $('[name=address]').val(data['display_name']); 
-                    mapContainer.flyTo([cordLat, corLong], 17);  
-                }); 
+                $.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${cordLat}&lon=${corLong}`, function(data) {
+                    $('[name=address]').val(data['display_name']);
+                    mapContainer.flyTo([cordLat, corLong], 17);
+                });
             });
 
 
-            mapContainer.on("dragend", function (e) {
+            mapContainer.on("dragend", function(e) {
 
                 var corLat = mapContainer.getCenter()['lat'];
                 var corLng = mapContainer.getCenter()['lng'];
                 var cord = `${corLat},${corLng}`;
 
-				$("[name=latitude]").val(corLat);
-				$("[name=longitude]").val(corLng);
+                $("[name=latitude]").val(corLat);
+                $("[name=longitude]").val(corLng);
                 $('[name=cordinate]').val(cord);
 
-                $.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${corLat}&lon=${corLng}`, function(data){
+                $.get(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${corLat}&lon=${corLng}`, function(data) {
 
-                    $('[name=address]').val(data['display_name']); 
+                    $('[name=address]').val(data['display_name']);
 
-                }); 
+                });
 
             });
-			
+
 
         });
 
@@ -269,13 +318,13 @@
                 {
                     data: 'category_fasum'
                 },
-				{
+                {
                     data: 'fasum_address'
                 },
-				{
+                {
                     data: 'fasum_phone'
                 },
-				{
+                {
                     data: 'jam_operasional',
                     orderable: false
                 },
@@ -301,7 +350,7 @@
 
     });
 
-	function detail(id) {
+    function detail(id) {
         $.ajax({
             url: '<?= base_url() ?>masterdata/Fasilitasumum/detailFasum/',
             type: 'POST',
@@ -310,7 +359,7 @@
             },
             dataType: 'JSON',
             success: function(results) {
-                
+
                 $('.DetailFasum,#namaFasum').attr('disabled', true)
                 $('.DetailFasum,#jenisFasum').attr('disabled', true)
                 $('.DetailFasum,#address').attr('disabled', true)
@@ -337,5 +386,4 @@
             }
         })
     }
-
 </script>
