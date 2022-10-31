2fd89a3422a93b1e8903bfff8226971618f776f5
gutaam.gt <gutaam.gt@gmail.com>
2022-10-31 12:06:53 +0800

    da

--- application/modules/masterdata/models/M_negara.php
+++ application/modules/masterdata/models/M_negara.php
@@ -115,9 +115,9 @@ class M_negara extends CI_Model {
             $row ['id']	=  $no++; 
             
             if($field['photo_country'] != null){
-                $row ['photo_country'] = '<img src="'.url_api().'country/'.$field['photo_country'].'" alt="" class="rounded-circle avatar-md">';  
+                $row ['photo_country'] = '<img src="'.url_api().'country/'.$field['photo_country'].'" alt="" class="rounded avatar-md">';  
             }else{
-                $row ['photo_country'] = '<img src="'.base_url().'assets/no_image.png" alt="" class="rounded-circle avatar-md">'; 
+                $row ['photo_country'] = '<img src="'.base_url().'assets/no_image.png" alt="" class="rounded avatar-md">'; 
             }
 
             $row ['name_country']	= $field['name_country'];  
