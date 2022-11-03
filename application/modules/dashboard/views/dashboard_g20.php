ee1cf47fa871b345eb29191bc31152e633780f46
gutaam.gt <gutaam.gt@gmail.com>
2022-11-03 15:40:43 +0800

    da

--- application/modules/dashboard/views/dashboard_g20.php
+++ application/modules/dashboard/views/dashboard_g20.php
@@ -3546,7 +3546,7 @@
                                                 <div class="col-md-12 col-12 mt-2">
                                                     <div class="row text-start">
                                                         <div class="col-md-12 col-12 text-start">
-                                                            <p style="font-size: 12px;font-weight: bold;">Keterangan</p>  
+                                                            <p style="font-size: 12px;font-weight: bold;">Akomodasi</p>  
                                                             <p style="font-size: 12px; margin-top: -13px;">${ressCluster[i].fasum_description.replace(/\n/g, "<br />")}</p>
                                                         </div>  
                                                          
