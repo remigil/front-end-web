13d50bb871c18ae27543359ff31612902d60894f
gutaam.gt <gutaam.gt@gmail.com>
2022-11-05 12:05:51 +0800

    da

--- application/modules/laporan/models/M_operasi.php
+++ application/modules/laporan/models/M_operasi.php
@@ -127,7 +127,7 @@ class M_operasi extends CI_Model
             $row['name_officer']      = $field['officer']['name_officer'];
             $row['categori']      = $field['categori'];
             $row['description']           = $field['description'];
-            $row['time']           =  gmdate($field['created_at']);
+            $row['time']           =  format_indoTglWkt($field['created_at']);
             $row['phone_officer']           = $field['officer']['phone_officer'];
             $row['status']           = $status;
             $row['action']             = '   
