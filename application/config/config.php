9e43b24c822b94f4bcb13b5914459249b3023ceb
gutaam.gt <gutaam.gt@gmail.com>
2022-11-05 21:30:01 +0800

    for um

--- application/config/config.php
+++ application/config/config.php
@@ -29,6 +29,7 @@ $config['modules_locations'] = array(
 // @$config['base_url']  = ((isset($_SERVER['HTTPS'])&& $_SERVER['HTTPS'] =="on") ? "https" : "http");
 // @$config['base_url'] .= "://" .$_SERVER['HTTP_HOST'];
 // @$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+
 $config['base_url'] = "http://34.143.227.90/";
 
 /*
