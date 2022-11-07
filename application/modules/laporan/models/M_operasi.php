5fab24db2ed7a628e8f8eeacbe42afdfc0b1e7d8
gutaam.gt <gutaam.gt@gmail.com>
2022-11-07 14:58:59 +0800

    tgl

--- application/modules/laporan/models/M_operasi.php
+++ application/modules/laporan/models/M_operasi.php
@@ -134,7 +134,7 @@ class M_operasi extends CI_Model
             $row['name_officer']      = $field['officer']['name_officer'];
             $row['categori']      = $field['categori'];
             $row['description']           = $field['description'];
-            $row['created_at']           =  format_indoTglWkt($field['created_at']);
+            $row['created_at']           =  $field['created_at'];
             $row['phone_officer']           = $field['officer']['phone_officer'];
             $row['status']           = $status;
             $row['action']             = '   
