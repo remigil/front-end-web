ede0d5d86f62381f5ed3c6820091e25c8a6eb329
aliwafa16 <aliwafamuhamad16@gmail.com>
2022-11-10 15:29:26 +0700

    asd

--- application/modules/dashboard/views/dashboard_eksekutif.php
+++ application/modules/dashboard/views/dashboard_eksekutif.php
@@ -630,7 +630,7 @@
 
                     $.ajax({
                         type: "POST",
-                        url: "<?php echo base_url(); ?>dashboard/getTurjagwali",
+                        url: "<?php echo base_url(); ?>Dashboard/getTurjagwali",
                         dataType: "JSON",
                         success: function(result) {
                             $("#overlay").fadeOut(300);
