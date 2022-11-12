b3d5dff1e82628ac650b2815e302f78d09db8b20
gutaam.gt <gutaam.gt@gmail.com>
2022-11-12 22:07:05 +0800

    da

---
+++ assets/fe/sejarah/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8" />
+        <title>K3I KORLANTAS</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesbrand" name="author" />
+
+    </head>
+
+    <body>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script>
+            $(document).ready(function() {  
+                window.location.href = `${window.location.origin}/404_notfound`;
+            });
+        </script> 
+    </body>
+
+</html>
