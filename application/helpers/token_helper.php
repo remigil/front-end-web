cdb92c7c462c938730991d3661cad62a662e4bd0
gutaam.gt <gutaam.gt@gmail.com>
2022-11-29 18:06:52 +0700

    for server

--- application/helpers/token_helper.php
+++ application/helpers/token_helper.php
@@ -7,8 +7,8 @@ if (!function_exists('token')) {
     function token()
     {
         $client = new Client();  
-        // $request = $client->request('POST', 'http://localhost:3020/v1/', [
-        $request = $client->request('POST', 'http://34.143.227.90:3020/v1/', [ 
+        // $request = $client->request('POST', 'http://localhost:3019/v1/', [
+        $request = $client->request('POST', 'http://34.143.227.90:3019/v1/', [ 
             'form_params' => [
                 'username' => 'Kakor',
                 'password' => 'a'
