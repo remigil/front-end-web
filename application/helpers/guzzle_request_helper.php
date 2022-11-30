a4c5954f82662b5a66b75301f9a09ac0dfd6da13
Ujang Irawan <ujangirawan@salamenterprise.com>
2022-12-01 02:09:48 +0700

    rollback base url to env uat

--- application/helpers/guzzle_request_helper.php
+++ application/helpers/guzzle_request_helper.php
@@ -17,8 +17,8 @@ if (!function_exists('guzzle_request')) {
     { 
         try {
             $client = new Client();
-            $request = $client->request($method, 'http://localhost:3002/v1/' . $uri, $body); 
-            // $request = $client->request($method, 'http://34.143.227.90:3019/v1/' . $uri, $body); 
+            // $request = $client->request($method, 'http://localhost:3002/v1/' . $uri, $body); 
+            $request = $client->request($method, 'http://34.143.227.90:3019/v1/' . $uri, $body); 
             $response = $request->getBody();
             $data = json_decode($response, true);
 
@@ -31,20 +31,20 @@ if (!function_exists('guzzle_request')) {
 
     function url_api()
     {
-        return 'http://localhost:3002/uploads/';
-        // return 'http://34.143.227.90:3019/uploads/'; 
+        // return 'http://localhost:3002/uploads/';
+        return 'http://34.143.227.90:3019/uploads/'; 
     }
 
     function urlApi()
     {
-        return 'http://localhost:3002/uploads/';
-        // return 'http://34.143.227.90:3019/uploads/'; 
+        // return 'http://localhost:3002/uploads/';
+        return 'http://34.143.227.90:3019/uploads/'; 
     }
 
     function ApiUrl()
     {
-        return 'http://localhost:3002/v1/';
-        // return 'http://34.143.227.90:3019/v1/';
+        // return 'http://localhost:3002/v1/';
+        return 'http://34.143.227.90:3019/v1/';
     }
 }
 
@@ -61,8 +61,8 @@ if (!function_exists('guzzle_requestTracking')) {
     {
         try {
             $client = new Client();
-            $request = $client->request($method, 'http://localhost:3002/track-location/' . $uri, $body);
-            // $request = $client->request($method, 'http://103.163.139.100:3005/track-location/' . $uri, $body);
+            // $request = $client->request($method, 'http://localhost:3002/track-location/' . $uri, $body);
+            $request = $client->request($method, 'http://103.163.139.100:3005/track-location/' . $uri, $body);
             $response = $request->getBody();
             $data = json_decode($response, true);
             return $data;
@@ -88,8 +88,8 @@ if (!function_exists('guzzle_requestAnev')) {
     {
         try {
             $client = new Client();
-            $request = $client->request($method, 'http://localhost:3002/' . $uri, $body);
-            // $request = $client->request($method, 'http://34.143.227.90:3019/' . $uri, $body);
+            // $request = $client->request($method, 'http://localhost:3002/' . $uri, $body);
+            $request = $client->request($method, 'http://34.143.227.90:3019/' . $uri, $body);
             $response = $request->getBody();
             $data = json_decode($response, true);
             return $data;
--- application/helpers/token_helper.php
+++ application/helpers/token_helper.php
@@ -7,8 +7,8 @@ if (!function_exists('token')) {
     function token()
     {
         $client = new Client();  
-        $request = $client->request('POST', 'http://localhost:3002/v1/', [
-        // $request = $client->request('POST', 'http://34.143.227.90:3019/v1/', [ 
+        // $request = $client->request('POST', 'http://localhost:3002/v1/', [
+        $request = $client->request('POST', 'http://34.143.227.90:3019/v1/', [ 
             'form_params' => [
                 'username' => 'Kakor',
                 'password' => 'a'
