18ccb05644c9346ecf92da196a74f120e7338e97
Muhamad Muslih Himawan <muslih259@gmail.com>
2022-12-04 21:47:01 +0700

    asd

--- application/modules/templates/views/app.php
+++ application/modules/templates/views/app.php
@@ -805,10 +805,10 @@
                             <?php $mobile = detect_mobile();
                             if ($mobile === true) { ?>
                                 <p style="font-size:10px; margin-bottom: 0px;margin-top: 10px;">Welcome to Dashboard Executive</p>
-                                <h5 style="display: flex;align-items: center;margin-left: 0px;margin-top: 5px; color: #787878; font-family:MortendBold; font-size:10px; margin-top:2px;" class="text-uppercase">PUSAT KENDALI KOORDINASI KOMUNIKASI KORLANTAS POLRI -&nbsp; <span style="color:#007DD8 ;"> <?= $this->session->userdata['full_name']; ?></span> </h5>
+                                <h5 style="display: flex;align-items: center;margin-left: 0px;margin-top: 5px; color: #787878; font-family:MortendBold; font-size:10px; margin-top:2px;" class="text-uppercase">PUSAT KENDALI KOORDINASI KOMUNIKASI & INFORMASI -&nbsp; <span style="color:#007DD8 ;"> <?= $this->session->userdata['full_name']; ?></span> </h5>
                             <?php } else { ?>
                                 <p style="font-size:15px; margin-bottom: 0px;margin-top: 10px;">Welcome to Dashboard Executive</p>
-                                <h3 style="display: flex;align-items: center;margin-left: 0px;margin-top: 5px; color:red; font-family:MortendBold; font-size:18px; margin-top:2px;" class="text-uppercase">PUSAT KENDALI KOORDINASI KOMUNIKASI KORLANTAS POLRI -&nbsp; <span style="color:#007DD8 ;"> K3I KORLANTAS POLRI <?= $this->session->userdata['name_operation'] ?></span> </h3>
+                                <h3 style="display: flex;align-items: center;margin-left: 0px;margin-top: 5px; color:red; font-family:MortendBold; font-size:18px; margin-top:2px;" class="text-uppercase">PUSAT KENDALI KOORDINASI KOMUNIKASI & INFORMASI -&nbsp; <span style="color:#007DD8 ;"> K3I KORLANTAS POLRI <?= $this->session->userdata['name_operation'] ?></span> </h3>
                                 <!-- <h3 style="display: flex;align-items: center;margin-left: 0px;margin-top: 5px; color:red; font-family:MortendBold; font-size:18px; margin-top:2px;" class="text-uppercase">PUSAT KENDALI KOORDINASI KOMUNIKASI KORLANTAS POLRI -&nbsp; <span style="color:#007DD8 ;"> <?= $this->session->userdata['full_name']; ?> <?= $this->session->userdata['name_operation'] ?></span> </h3> -->
 
                                 <!-- <p style="margin-bottom: 0px;margin-top: 10px;">Welcome to Dashboard Executive</p>
@@ -1095,7 +1095,7 @@
                                         <li><a href="<?php echo base_url('masterdata/kepemilikan'); ?>" data-key="t-read-email">Kepemilikan Kendaraan</a></li>
                                         <li><a href="<?php echo base_url('masterdata/bahan_bakar'); ?>" data-key="t-read-email">Bahan Bakar Kendaraan</a></li>
                                         <li><a href="<?php echo base_url('masterdata/Fasilitasumum'); ?>" data-key="t-read-email">Fasilitas Khusus</a></li>
-                                        <li><a href="<?php echo base_url('masterdata/Fasilitasumum/radius'); ?>" data-key="t-read-email">Cluster</a></li> 
+                                        <li><a href="<?php echo base_url('masterdata/Fasilitasumum/radius'); ?>" data-key="t-read-email">Cluster</a></li>
                                         <li><a href="<?php echo base_url('masterdata/Fasilitasumum/restArea'); ?>" data-key="t-read-email">Rest Area</a></li>
                                         <li><a href="<?php echo base_url('masterdata/Fasilitasumum/posPam'); ?>" data-key="t-read-email">POS PAM</a></li>
                                         <li><a href="<?php echo base_url('masterdata/Fasilitasumum/satPas'); ?>" data-key="t-read-email">SATPAS</a></li>
